12393d67cbe23080c6c945560e43af4b5142c857
jamesjoyce216 <34312834+jamesjoyce216@users.noreply.github.com>
2019-05-02 11:25:17 -0500

    Add files via upload

---
+++ p4JamesJoyce_1/src/p3jamesjoyce/p3JamesJoyce.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class p3JamesJoyce {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        //Get name of first bidder
+        System.out.print("Enter First Bidder's Name: ");
+        String name1 = input.next();
+        //Get first bidder's charge per hour
+        
+    }
+    
+}
